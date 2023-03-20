9c9eb369643cac7242e984e10947c4e3182ef07c
tinatrinh1012 <68746212+tinatrinh1012@users.noreply.github.com>
2023-03-19 22:52:18 -0500

    get random dog pics

--- App.tsx
+++ App.tsx
@@ -1,11 +1,36 @@
 import * as React from 'react';
 import './style.css';
+import { useState, useEffect } from 'react';
 
 export default function App() {
+  type Dog = {
+    message: string;
+  };
+
+  const [data, setData] = useState<Dog>({} as Dog);
+
+  useEffect(() => {
+    fetchDogPic();
+  }, []);
+
+  function fetchDogPic() {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setData(data);
+      })
+      .catch((err) => console.log(err));
+  }
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>React + Typescript App</h1>
+      <p>Random dog pic</p>
+      <div id="pic_container">
+        <img src={data.message} alt="" />
+      </div>
+      <button onClick={fetchDogPic}>Get random dog pic</button>
     </div>
   );
 }
