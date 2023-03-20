06c7c9f3cba5ce4faa2e79dbd58d307a9287b67c
tinatrinh1012 <68746212+tinatrinh1012@users.noreply.github.com>
2023-03-19 23:32:17 -0500

    random cat pic generator

--- App.tsx
+++ App.tsx
@@ -6,19 +6,34 @@ export default function App() {
   type Dog = {
     message: string;
   };
+  type Cat = {
+    url: string;
+  };
 
-  const [data, setData] = useState<Dog>({} as Dog);
+  const [dog, setDog] = useState<Dog>({} as Dog);
+  const [cat, setCat] = useState<Cat>({} as Cat);
 
   useEffect(() => {
     fetchDogPic();
+    fetchCatPic();
   }, []);
 
   function fetchDogPic() {
     fetch('https://dog.ceo/api/breeds/image/random')
       .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setData(data);
+      .then((dogData) => {
+        console.log(dogData);
+        setDog(dogData);
+      })
+      .catch((err) => console.log(err));
+  }
+
+  function fetchCatPic() {
+    fetch('https://api.thecatapi.com/v1/images/search')
+      .then((res) => res.json())
+      .then((catData) => {
+        console.log(catData[0]);
+        setCat(catData[0]);
       })
       .catch((err) => console.log(err));
   }
@@ -26,9 +41,16 @@ export default function App() {
   return (
     <div>
       <h1>React + Typescript App</h1>
+
+      <p>Random cat pic</p>
+      <div className="pic_container">
+        <img src={cat.url} alt="" />
+      </div>
+      <button onClick={fetchCatPic}>Get random cat pic</button>
+
       <p>Random dog pic</p>
-      <div id="pic_container">
-        <img src={data.message} alt="" />
+      <div className="pic_container">
+        <img src={dog.message} alt="" />
       </div>
       <button onClick={fetchDogPic}>Get random dog pic</button>
     </div>
