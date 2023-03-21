360df5f5411408bf22ed9e7e2360b499ee7b0bb4
tinatrinh1012 <68746212+tinatrinh1012@users.noreply.github.com>
2023-03-20 23:07:13 -0500

    add router

--- App.tsx
+++ App.tsx
@@ -1,58 +1,11 @@
 import * as React from 'react';
-import './style.css';
-import { useState, useEffect } from 'react';
+// import './style.css';
 
 export default function App() {
-  type Dog = {
-    message: string;
-  };
-  type Cat = {
-    url: string;
-  };
-
-  const [dog, setDog] = useState<Dog>({} as Dog);
-  const [cat, setCat] = useState<Cat>({} as Cat);
-
-  useEffect(() => {
-    fetchDogPic();
-    fetchCatPic();
-  }, []);
-
-  function fetchDogPic() {
-    fetch('https://dog.ceo/api/breeds/image/random')
-      .then((res) => res.json())
-      .then((dogData) => {
-        console.log(dogData);
-        setDog(dogData);
-      })
-      .catch((err) => console.log(err));
-  }
-
-  function fetchCatPic() {
-    fetch('https://api.thecatapi.com/v1/images/search')
-      .then((res) => res.json())
-      .then((catData) => {
-        console.log(catData[0]);
-        setCat(catData[0]);
-      })
-      .catch((err) => console.log(err));
-  }
-
   return (
     <div>
       <h1>React + Typescript App</h1>
-
-      <p>Random cat pic</p>
-      <div className="pic_container">
-        <img src={cat.url} alt="" />
-      </div>
-      <button onClick={fetchCatPic}>Get random cat pic</button>
-
-      <p>Random dog pic</p>
-      <div className="pic_container">
-        <img src={dog.message} alt="" />
-      </div>
-      <button onClick={fetchDogPic}>Get random dog pic</button>
+      <h3>Home Page</h3>
     </div>
   );
 }
--- index.tsx
+++ index.tsx
@@ -1,14 +1,27 @@
 import * as React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from './App';
+import RandomCatDog from './src/pages/RandomCatDog';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/random-cat-dog',
+    element: <RandomCatDog />,
+  },
+]);
+
 root.render(
   // <StrictMode>
-  <App />
+  <RouterProvider router={router} />
   // </StrictMode>
 );
