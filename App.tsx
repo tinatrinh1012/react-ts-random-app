5d7e827763fcf8b4c8e0347b5b06849acd3b7a4c
tinatrinh1012 <68746212+tinatrinh1012@users.noreply.github.com>
2023-03-21 18:44:42 -0500

    typescript type example

--- App.tsx
+++ App.tsx
@@ -1,5 +1,67 @@
 import * as React from 'react';
-// import './style.css';
+
+/* TYPE vs. INTERFACE */
+type Person = {
+  name: string;
+  age?: number;
+};
+interface Student extends Person {
+  name: string;
+  age?: number;
+}
+interface Teacher extends X {
+  name: string;
+  age: number;
+}
+type X = {
+  a: string;
+  b: number;
+};
+type Y = X & {
+  c: string;
+  d: number;
+};
+type Z = Person & {
+  e: string;
+  f: number;
+};
+
+/* DECLARE VARIABLE TYPE */
+let name: any;
+let personName: unknown;
+let age: number | string;
+let isStudent: boolean;
+let hobbies: string[];
+
+// tuple
+let role: [number, string];
+
+// object
+let person: Person = {
+  name: 'Tina',
+};
+
+// array
+let lotsOfPeople: Person[];
+
+// function
+let printNameFunction: (name: string) => never; // 'never' doesn't return anything, while 'void' returns an undefined
+
+/* EXAMPLE */
+// using defined type
+let y: Y = {
+  // has to include X type attribute
+  c: 'asdf',
+  d: 123,
+  a: 'jk;',
+  b: 234,
+};
+
+function printName(name: string) {
+  console.log(name);
+}
+
+role = [5, 'tina'];
 
 export default function App() {
   return (
