b0df23ae1de9eb4cde951fe37fd082a4a9291f71
tinatrinh1012 <68746212+tinatrinh1012@users.noreply.github.com>
2023-03-21 21:50:15 -0500

    handle adding item to todos list

--- src/pages/TodoApp/TodoApp.tsx
+++ src/pages/TodoApp/TodoApp.tsx
@@ -1,12 +1,28 @@
 import * as React from 'react';
+import { useState } from 'react';
+import { Todo } from '../TodoApp/model';
 import InputField from './components/InputField';
 import './TodoApp.css';
 
 const TodoApp: React.FC = () => {
+  const [todo, setTodo] = useState<string>('');
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }]);
+      setTodo('');
+    }
+  };
+
+  // console.log(todos);
+
   return (
     <div id="todo-app">
       <div className="heading">Taskify</div>
-      <InputField />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
     </div>
   );
 };
